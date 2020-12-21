a7b75e8ae926b658e7dce14edc56914a6ac4e829
Gaurav bl <gaurav12bl@gmail.com>
2020-12-21 14:44:58 +0530

    update on webapp

--- api/main.py
+++ api/main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import sqlite3
 from database import initdb
 
 initdb()
 
 app = Flask(__name__)
+app.secret_key = "secret"
 
 @app.route('/')
 @app.route('/index')
@@ -29,3 +30,50 @@ def signup():
         c.execute("""INSERT INTO USERS(USER_ID, EMAIL, PASSWORD) VALUES(?,?,?);""", (user_name, email, password))
 
     return "success"
+
+
+@app.route('/signin')
+def signin_page():
+    return render_template('signin.html')
+
+@app.route('/signin_button', methods=['POST'])
+def signin():
+    if "email" in session:
+        return redirect('/dashboard')
+    else:
+        cred ={
+            "usr":request.form.get("username"),
+            "pwd":request.form.get("password")
+        }
+        with sqlite3.connect('podcast.db') as conn:
+            c=conn.cursor()
+
+            query = c.execute("""SELECT email
+                        FROM USERS
+                        WHERE USER_ID=(?) AND PASSWORD=(?)""", tuple(cred.values())).fetchone()
+
+            if query is not None:
+                session['email'] = query[0]
+                return redirect('/dashboard')
+            else:
+                return "failure"
+
+@app.route('/signout')
+def signout():
+    if "email" in session:
+        session.pop("email", None)
+        print("User has signed out")
+        return "successfully signed out"
+
+@app.route('/dashboard')
+def dashboard():
+    # if "email" in session:
+    #     with sqlite3.connect('podcast.db') as conn:
+    #         c =conn.cursor()
+    #         c.execute("""SELECT * FROM PODCAST""")
+    return render_template('dashboard.html')
+        
+
+    
+
+        
